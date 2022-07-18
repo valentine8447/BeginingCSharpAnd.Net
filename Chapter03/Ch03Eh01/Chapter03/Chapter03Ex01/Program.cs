fcadb3ebcb8a2db5940fe1afe1f48930c7f22f47
valentine8447 <99423767+valentine8447@users.noreply.github.com>
2022-07-18 13:55:59 -0700

    comitic1

---
+++ Chapter03/Ch03Eh01/Chapter03/Chapter03Ex01/Program.cs
@@ -0,0 +1,12 @@
+﻿
+static void Main(String[] args)
+{
+    int myInteger;
+    string myString;
+    myInteger = 17;
+    myString = "\"myInteger\" is";
+    Console.WriteLine($"{myString} {myInteger}");
+   
+}
+
+
